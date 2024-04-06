c763d5a53b3863c776e5af7fdb5296ff920649d6
Luis Barrera <109974057+YoSoyLuis503@users.noreply.github.com>
2024-04-05 20:59:58 -0600

    hadwuihahidwaha

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,11 +6,7 @@
 <div class="container mt-5">
   
 <div class="container w-25 border p-4 mt-4"> 
-<<<<<<< HEAD
-      Nombre y codigo:
-=======
   Nombre y codigo
->>>>>>> 717069f797606fb5a6b4dc8134a4666c93a6a907
 </div>
 
 <div class="container w-25 border p-4 mt-4"> 
