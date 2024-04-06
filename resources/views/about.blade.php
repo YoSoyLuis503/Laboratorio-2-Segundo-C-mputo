5989bc37704ee10958f490eca40009395faaa921
Luis Barrera <109974057+YoSoyLuis503@users.noreply.github.com>
2024-04-05 19:08:15 -0600

    version 1
    hola

---
+++ resources/views/about.blade.php
@@ -0,0 +1,30 @@
+
+@extends('home')
+
+@section('content')
+
+<div class="container mt-5">
+  
+<div class="container w-25 border p-4 mt-4"> 
+      Nombre y codigo
+</div>
+
+<div class="container w-25 border p-4 mt-4"> 
+  Nombre y codigo
+</div>
+  
+<div class="container w-25 border p-4 mt-4"> 
+  Nombre y codigo
+</div>
+
+<div class="container w-25 border p-4 mt-4"> 
+  Nombre y codigo
+</div>
+
+<div class="container w-25 border p-4 mt-4"> 
+  Nombre y codigo
+</div>
+
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/nayik', function () {
+    return view('home');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
