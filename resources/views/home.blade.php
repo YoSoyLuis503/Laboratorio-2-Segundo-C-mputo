99658e0f112514d0c2ea1f5613be1a174fff55b1
RyanEsc <110208605+Bybryanx1997@users.noreply.github.com>
2024-04-07 08:08:19 -0600

    Update home.blade.php
    Footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -96,5 +96,8 @@
     <!-- Incluye el contenido dinámico aquí -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
+    <footer>
+        <p>© 2024 TEAM CHERRY. Todos los derechos reservados.</p>
+    </footer>
+  </body>
 </html>
